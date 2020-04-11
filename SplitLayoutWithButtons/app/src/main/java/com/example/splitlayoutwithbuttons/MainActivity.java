ade7d27e5f06aeb622249a8fab7df9dd9d0d7de6
pradeep-charism <pradeep.charism@gmail.com>
2020-04-12 01:40:14 +0800

    Liner and Relative layout together

--- SplitLayoutWithButtons/app/src/main/java/com/example/splitlayoutwithbuttons/MainActivity.java
+++ SplitLayoutWithButtons/app/src/main/java/com/example/splitlayoutwithbuttons/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.splitlayoutwithbuttons;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -13,7 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // but the summary slide at the end of the video mentions that possibility.
-        Button button_big = (Button) findViewById(R.id.button_big);
+        Button button_big = findViewById(R.id.button_big);
         button_big.setBackgroundResource(R.drawable.compass);
+
+        Button button_small = findViewById(R.id.button_small);
+        button_small.setBackgroundColor(Color.MAGENTA);
     }
 }
