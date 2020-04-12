84e0ac316c7041fffccedb70bb5218d586a5a3f7
pradeep-charism <pradeep.charism@gmail.com>
2020-04-12 19:32:01 +0800

    Number picker and webview

--- NumberPickerAndWebPage/app/src/main/java/com/example/numberpickerandwebpage/MainActivity.java
+++ NumberPickerAndWebPage/app/src/main/java/com/example/numberpickerandwebpage/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.numberpickerandwebpage;
 
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.NumberPicker;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    // This NumberPicker allows the user to choose the web page to display.
+    private NumberPicker numberPicker;
+    // webView to display the web pages
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // display the GUI defined in the activity_main.xml file
         setContentView(R.layout.activity_main);
+
+        // retrieve references to the Views defined in the activity_main.xml
+        numberPicker = findViewById(R.id.numberPicker);
+        webView = findViewById(R.id.webView);
+        String[] possibilitiesStrings = getItemsForPicker();
+
+        // associate the defined texts to the number picker
+        numberPicker.setDisplayedValues(possibilitiesStrings);
+
+        // the number picker offers 4 choices, indexed from 0 to 3
+        // (length of the array of strings is 4)
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(possibilitiesStrings.length - 1);
+    }
+
+    private String[] getItemsForPicker() {
+        // define the texts to be displayed by the number picker
+        return new String[]{
+                "Android",
+                "Checklist text-input field",
+                "Coursera",
+                "Supélec"
+        };
+    }
+
+    /**
+     * called when the OK button from activity_main.xml is clicked
+     *
+     * @param v the View which triggered the method call: the OK button
+     */
+    public void navigate(View v) {
+        // retrieve the index of the user's choice on the number picker
+        int choice = numberPicker.getValue();
+        // when a link is clicked in the selected page, do not open a browser to display
+        // the linked page, instead display it in the (same) web view (replacing
+        // the initially selected page)
+//        webView.setWebViewClient(new WebViewClient());
+        // load the appropriate web page
+        if (choice == 0) {
+            // load the android.html page stored in the assets of the application
+            webView.loadUrl("file:///android_asset/android.html");
+        } else if (choice == 1)
+            // load the checklist.html page stored in the assets of the application
+            webView.loadUrl("file:///android_asset/checklist.html");
+        else if (choice == 2)
+            // load the coursera web page from the Internet
+            // -> require modification to the AndroidManisfest.xml file:
+            //  <uses-permission android:name="android.permission.INTERNET"/>
+            webView.loadUrl("http://www.coursera.org");
+        else if (choice == 3)
+            // load the supelec.html page stored in the assets of the application
+            webView.loadUrl("file:///android_asset/supelec.html");
     }
 }
