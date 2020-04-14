4821ab49f6c3f57a7e1c0b9135a702334032c0e3
pradeep-charism <pradeep.charism@gmail.com>
2020-04-15 00:33:16 +0800

    New project to pass values between activities

--- PassingValuesBetweenActivities/app/src/main/java/com/example/passingvaluesbetweenactivities/MainActivity.java
+++ PassingValuesBetweenActivities/app/src/main/java/com/example/passingvaluesbetweenactivities/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.passingvaluesbetweenactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enterRating(View v){
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        float rating = ratingBar.getRating();
+        Toast.makeText(getApplicationContext(), "Selected rating: "+ rating, Toast.LENGTH_SHORT).show();
+
     }
 }
