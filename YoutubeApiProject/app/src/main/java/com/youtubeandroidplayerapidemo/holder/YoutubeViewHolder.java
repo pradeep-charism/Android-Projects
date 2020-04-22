9cb0405d55eff35882a2c17c9e79b67c0e177559
pradeep-charism <pradeep.charism@gmail.com>
2020-04-23 01:06:39 +0800

    Youtube Video demo working

---
+++ YoutubeApiProject/app/src/main/java/com/youtubeandroidplayerapidemo/holder/YoutubeViewHolder.java
@@ -0,0 +1,26 @@
+package com.youtubeandroidplayerapidemo.holder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.youtube.player.YouTubeThumbnailView;
+import com.youtubeandroidplayerapidemo.R;
+
+/**
+ * Created by sonu on 10/11/17.
+ */
+
+public class YoutubeViewHolder extends RecyclerView.ViewHolder {
+
+    public YouTubeThumbnailView videoThumbnailImageView;
+    public TextView videoTitle, videoDuration;
+
+    public YoutubeViewHolder(View itemView) {
+        super(itemView);
+        videoThumbnailImageView = itemView.findViewById(R.id.video_thumbnail_image_view);
+        videoTitle = itemView.findViewById(R.id.video_title_label);
+        videoDuration = itemView.findViewById(R.id.video_duration_label);
+    }
+}
